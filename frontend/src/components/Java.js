199b4727b0111eaffbb87c7c729305c60e850194
Hikmet Çakır <hikmet_cakir@outlook.com>
2023-03-29 00:33:05 +0300

    Add semantic ui entegration

--- frontend/src/components/Java.js
+++ frontend/src/components/Java.js
@@ -1,10 +1,23 @@
 import Header from './Header';
+import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
+
 
 function Java() {
+  const description = [
+    'Amy is a violinist with 2 years experience in the wedding industry.',
+    'She enjoys the outdoors and currently resides in upstate New York.',
+  ].join(' ')
   return (
     <java>
         <Header/>
-
+          <Card>
+            <Card.Content header='Reflection API' />
+            <Card.Content description={description} />
+            <Card.Content extra>
+            <Icon name='angle right'>Go</Icon>
+            </Card.Content>
+      </Card>
     </java>
   );
 }
