f50c319e841d51c78043a17c35d174d40a323e7d
Hikmet Çakır <hikmet_cakir@outlook.com>
2023-04-04 20:01:00 +0300

    Improve Java page

--- frontend/src/components/Java.js
+++ frontend/src/components/Java.js
@@ -1,23 +1,41 @@
 import Header from './Header';
-import React from 'react'
-import { Card, Icon } from 'semantic-ui-react'
+import React from "react";
+import axios from "axios";
+import { Card, Container, Icon } from 'semantic-ui-react'
 
 
 function Java() {
+  const [post, setPost] = React.useState(null);
+  const baseURL = "http://localhost:8080/article?id=9c1e2c06-5790-438c-b757-dd7adf9dce3f";
+
   const description = [
     'Amy is a violinist with 2 years experience in the wedding industry.',
     'She enjoys the outdoors and currently resides in upstate New York.',
   ].join(' ')
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+  
   return (
     <java>
-        <Header/>
+        <Header/> 
+        <Container> 
+        <div class="ui centered fluid card"> 
           <Card>
             <Card.Content header='Reflection API' />
             <Card.Content description={description} />
             <Card.Content extra>
             <Icon name='angle right'>Go</Icon>
             </Card.Content>
-      </Card>
+          </Card>
+      </div>
+      </Container>
     </java>
   );
 }
