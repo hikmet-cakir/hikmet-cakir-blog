3318158e52c2fa5b6da88ddb8038765c6ae6fa0c
Hikmet Çakır <hikmet_cakir@outlook.com>
2024-05-11 18:03:26 +0300

    Change Article class with record

--- backend/src/main/java/com/hikmetcakir/dto/Article.java
+++ backend/src/main/java/com/hikmetcakir/dto/Article.java
@@ -1,34 +1,24 @@
 package com.hikmetcakir.dto;
 
 import com.hikmetcakir.entity.ArticleEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Article {
+public record Article(String id,
+                      String title,
+                      String content,
+                      String topic,
+                      LocalDate updatedDate,
+                      LocalDate createdDate) {
 
-    private String id;
-    private String title;
-    private String content;
-    private String topic;
-    private LocalDate updatedDate;
-    private LocalDate createdDate;
-
-    public static Article from(ArticleEntity articleEntity) {
-        Article article = new Article();
-        article.setId(articleEntity.getId());
-        article.setTitle(articleEntity.getTitle());
-        article.setContent(articleEntity.getContent());
-        article.setTopic(articleEntity.getTopic());
-        article.setUpdatedDate(articleEntity.getUpdatedDate());
-        article.setCreatedDate(articleEntity.getCreatedDate());
-        return article;
+    public Article(ArticleEntity articleEntity) {
+        this(
+                articleEntity.getId(),
+                articleEntity.getTitle(),
+                articleEntity.getContent(),
+                articleEntity.getTopic(),
+                articleEntity.getUpdatedDate(),
+                articleEntity.getCreatedDate()
+        );
     }
 }
